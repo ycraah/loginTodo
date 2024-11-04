1560f75abc836ec35b6e08119b1de0dd89ed9856
김예찬 <ycraah@gamil.com>
2024-11-04 15:01:46 +0900

    "엔티티 중 공통속성(날짜)를 자동으로 작성되도록 설정 "

--- src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
+++ src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
@@ -3,10 +3,12 @@ package com.ycraah.springbootboard;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 import java.util.Properties;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootBoardApplication {
   public static void main(String[] args) {
     SpringApplication.run(SpringBootBoardApplication.class, args);
---
+++ src/main/java/com/ycraah/springbootboard/domain/baseEntity.java
@@ -0,0 +1,24 @@
+package com.ycraah.springbootboard.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Getter
+public class baseEntity {
+  @CreatedDate
+  @Column(name = "regDate", updatable = false)
+  private LocalDateTime regDate;
+
+  @LastModifiedDate
+  @Column(name="modDate")
+  private LocalDateTime modDate;
+}
