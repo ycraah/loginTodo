133808ef5a370ac88c3ff1416ff2bf07d39706cd
김예찬 <ycraah@gamil.com>
2024-11-04 11:14:59 +0900

    테스트 호ë환경에서 Lombok을 사용하기 위한 ì의존성 추가

--- src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
+++ src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootBoardApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(SpringBootBoardApplication.class, args);
   }
-
 }
