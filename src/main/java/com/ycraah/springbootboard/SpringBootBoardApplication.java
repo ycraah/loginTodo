4a0b18b4fb6f01c3b21ef57fffa51c1f8efad026
김예찬 <ycraah@gamil.com>
2024-11-04 11:48:05 +0900

    컨트롤러, 타임리프 사용(테스트완)

--- src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
+++ src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Properties;
 
-@Log4j2
 @SpringBootApplication
 public class SpringBootBoardApplication {
   public static void main(String[] args) {
