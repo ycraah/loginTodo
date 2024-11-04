37b7cf3f39c5bf7c0cab6626f6814d6de5b9762a
김예찬 <ycraah@gamil.com>
2024-11-05 00:20:21 +0900

    "QueryDSL을 사용하기 위한 세팅, Q클래스 확인"

--- src/main/java/com/ycraah/springbootboard/repository/BoardRepository.java
+++ src/main/java/com/ycraah/springbootboard/repository/BoardRepository.java
@@ -2,7 +2,9 @@ package com.ycraah.springbootboard.repository;
 
 import com.ycraah.springbootboard.domain.Board;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface BoardRepository extends JpaRepository<Board, Long> {
+public interface BoardRepository extends JpaRepository<Board, Long>, QuerydslPredicateExecutor<Board> {
 
 }
