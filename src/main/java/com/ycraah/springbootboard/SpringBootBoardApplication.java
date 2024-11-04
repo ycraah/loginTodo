f5723ee07cad37b7e4e0d70ed8d1f96f8e82eca5
김예찬 <ycraah@gamil.com>
2024-11-04 11:24:20 +0900

    로그를 별로 파일로 설정

--- src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
+++ src/main/java/com/ycraah/springbootboard/SpringBootBoardApplication.java
@@ -1,11 +1,17 @@
 package com.ycraah.springbootboard;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
+@Log4j2
 @SpringBootApplication
 public class SpringBootBoardApplication {
   public static void main(String[] args) {
+    Properties properties = new Properties();
+    log.info(properties);
     SpringApplication.run(SpringBootBoardApplication.class, args);
   }
 }
