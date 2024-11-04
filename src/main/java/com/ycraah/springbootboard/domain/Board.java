902d2db258f3ffd709a41cdb9724cb7060ae88fe
김예찬 <ycraah@gamil.com>
2024-11-04 14:41:16 +0900

    "Board 엔티티 객체 생성"

---
+++ src/main/java/com/ycraah/springbootboard/domain/Board.java
@@ -0,0 +1,16 @@
+package com.ycraah.springbootboard.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Board {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long bno;
+  private String title;
+  private String content;
+  private String writer;
+}
